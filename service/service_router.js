c0618feb0bc7e16b74c88444b5baad6b53b33b77
WorldFS <fsworld009@gmail.com>
2016-11-06 12:10:06 -0500

    README, some cleanup

--- service/service_router.js
+++ service/service_router.js
@@ -6,10 +6,10 @@ var fs = require("fs");
 Promise.promisifyAll(fs);
 
 var basePath = path.resolve(__dirname, "data/");
-router.use(function timeLog (req, res, next) {
+/*router.use(function timeLog (req, res, next) {
   console.log('Time: ', Date.now());
   next();
-});
+});*/
 router.get("/headings/", function(req, res, next){
     fs.readFileAsync(basePath + "/headings.json","utf-8").then(function(data){
         res.setHeader('Content-Type', 'application/json');
@@ -17,7 +17,7 @@ router.get("/headings/", function(req, res, next){
         //return Promise.resolve();
     });
     /*},function(err){
-        return fs.readFileAsync(basePath + "/profiles/default.json","utf-8");
+        return fs.readFileAsync(basePath + "/default.json","utf-8");
     }).then(function(data){
         if(data){
             responseJson = data;
